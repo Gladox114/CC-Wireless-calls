7ec2061c30080aac02721b718e1ee6e3a4e084ad
Gladox114 <Gladox114@github.com>
2021-03-18 03:52:16 +0100

    test release

--- Tablet.lua
+++ Tablet.lua
@@ -4,8 +4,8 @@ modem.open(442)
 x,y = term.getSize()
 
 function lastMessage(text)
-    term.clearLine(y)
     term.setCursorPos(1,y)
+    term.clearLine(y)
     term.write(text)
 end
 
@@ -17,7 +17,7 @@ term.setCursorPos(1,3)
 term.write("S: Down")
 term.setCursorPos(1,4)
 term.write("F: Toggle Stop/Continue")
-lastMessage("test")
+lastMessage("<LASTMESSAGE|DEBUG>")
 
 local toggle1 = true
 
@@ -25,14 +25,14 @@ while true do
     -- arg1 = keynum, arg2 = key_pressed?
     -- arg1 = modemSide, arg2 = senderChannel, arg3 = replyChannel, arg4 = message, arg5 = senderDistance
     local Event_type, arg1, arg2, arg3, arg4, arg5 = os.pullEvent()
-    if Event_type == "key" then
-        if arg1 == "87" then -- W
+    if Event_type == "char" then
+        if arg1 == "w" then
             modem.transmit(442,442,"UP")
             lastMessage("Send: UP")
-        elseif arg1 == "83" then -- S
+        elseif arg1 == "s" then
             modem.transmit(442,442,"DOWN")
             lastMessage("Send: DOWN")
-        elseif arg1 == "70" then -- F
+        elseif arg1 == "f" then
             if toggle1 then
                 modem.transmit(442,442,"STOP")
                 lastMessage("Send: STOP")
